7f5d66ff1c234453b7899a67f3bbaa8e46a376e6
izluu <163074579+izluu@users.noreply.github.com>
2024-08-16 15:06:17 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+const btnGroup = document.querySelector(".btn-group");
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "I love you too! 😘";
+  gif.src = "2.webp";
+  
+  yesBtn.style.display = "none";
+  noBtn.style.display = "none";
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const groupRect = btnGroup.getBoundingClientRect();
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = groupRect.width - noBtnRect.width;
+  const maxY = groupRect.height - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.position = 'absolute'; // Chuyển vị trí nút No sang vị trí tuyệt đối khi di chuyển
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
